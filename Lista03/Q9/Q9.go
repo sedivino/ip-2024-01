0080228b71e902dd68c9791186d6b0cfc68d9fd9
Eduardo Divino <eduardo.divino@discente.ufg.br>
2024-05-14 20:22:40 -0300

    up

---
+++ Lista03/Q9/Q9.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Declaração de variáveis:
+	const N = 3
+
+	// Leitura dos pontos A, B e C:
+	// Ponto A:
+	a := [3]int{}
+	
+	fmt.Println("Digite as coordenadas do ponto A(x,y,z): ")
+	for i := 0; i < N; i++ {
+		fmt.Scan(&a[i])
+	}
+
+	// Ponto B:
+	b := [3]int{}
+	
+	fmt.Println("Digite as coordenadas do ponto B(x,y,z): ")
+	for i := 0; i < N; i++ {
+		fmt.Scan(&b[i])
+	}
+		
+	fmt.Print(a)
+	fmt.Print(b)
+	
+}
