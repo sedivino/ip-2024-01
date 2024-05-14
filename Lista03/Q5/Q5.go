e6ae7c52825736a65439cb92c23ca029f6eae1c2
Eduardo Divino <eduardo.divino@discente.ufg.br>
2024-05-14 15:01:16 -0300

    up

--- Lista03/Q5/Q5.go
+++ Lista03/Q5/Q5.go
@@ -1,32 +1,50 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-
-	// Declaração de variáveis
-	var n int
-
-	for
-	
-	
 	for {
-		
-		fmt.Print("Digite a quantidade de elementos do vetor: ")
+		// Declaração de variáveis:
+		var n int
+
+		// Lendo o tamanho do vetor
+		fmt.Print("Digite o tamanho do vetor: ")
 		fmt.Scan(&n)
-		
-		if n != 0 {
 
-			v := make([]int, n)
-
-			for i := 0; i < n; i++ {
-				fmt.Scan(&v[i])
-			}
-		} else {
+		// Verificando se o tamanho é zero
+		if n == 0 {
 			break
 		}
+
+		// Criando o vetor e lendo seus elementos
+		v := make([]int, n)
+
+		fmt.Print("Digite os elementos do vetor: ")
+
+		for i := 0; i < n; i++ {
+			fmt.Scan(&v[i])
+		}
+
+		// Encontrando o maior elemento e o índice da primeira ocorrência
+		maiorElemento, indice := encontrarMaiorElemento(v)
+
+		// Imprimindo o resultado
+		fmt.Println(indice, maiorElemento)
 	}
-	
+}
+
+func encontrarMaiorElemento(vetor []int) (int, int) {
+	// Inicializando o maior elemento e o índice da primeira ocorrência
+	maiorElemento := vetor[0]
+	indice := 0
+
+	// Iterando pelo vetor para encontrar o maior elemento e seu índice
+	for i, elemento := range vetor {
+		if elemento > maiorElemento {
+			maiorElemento = elemento
+			indice = i
+		}
+	}
+
+	return maiorElemento, indice
 }
