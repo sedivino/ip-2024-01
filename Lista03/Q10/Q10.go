31aef7ea615556cb0c6c7ba7f48ecd23b552c577
Eduardo Divino <eduardo.divino@discente.ufg.br>
2024-05-14 21:32:15 -0300

    ta quase

--- Lista03/Q10/Q10.go
+++ Lista03/Q10/Q10.go
@@ -13,24 +13,39 @@ func main() {
 	fmt.Print("Digite a quantidade de notas: ")
 	fmt.Scan(&n)
 
-	// Verificando a quantidade de notas
-	for {
-		if n <= 1 || n >= 10000 {
-			fmt.Print("Digite uma quantidade entre 1 e 10000: ")
-			fmt.Scan(&n)
-		} else {
-			break
-		}
-	}
-
-	// Recebendo as notas:
+	// Recebendo as notas e identificando o maior elemento
 	notas := make([]int, n)
-
+	
 	fmt.Print("Digite as notas: ")
 		
 	for i := 0; i < n; i++ {
 		fmt.Scan(&notas[i])
 	}
+	
+	// Identificando o último elemento e fazendo sua contagem:
+	ultimo := notas[len(notas)-1]
+	contagem := 0 
 
-	fmt.Print(notas)
+	for _, elemento := range notas {
+		if elemento == ultimo {
+			contagem++
+		}
+	}
+
+	notaMax := encontrarMaiorElemento(notas)
+
+
+	// Imprimindo os resultados:
+	fmt.Printf("Nota %d, %d vezes\n", ultimo, contagem)
+	fmt.Printf("Nota %d, índice ", notaMax)
+}
+
+func encontrarMaiorElemento(vetor []int) int {
+	maior := vetor[0]
+	for _, num := range vetor {
+		if num > maior {
+			maior = num
+		}
+	}
+	return maior
 }
