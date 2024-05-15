40b1fa0e0b0ddc02223aaa953e3939edcca47aae
Eduardo Divino <eduardo.divino@discente.ufg.br>
2024-05-15 11:24:17 -0300

    up

---
+++ Lista03/Q12/Q12.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	// Declaração das variáveis
+	var n int
+
+	// Leitura do tamanho do vetor
+	fmt.Print("Digite o tamanho da lista de números: ")
+	fmt.Scan(&n)
+
+	// Verificando o tamanho da lista:
+	for {
+		if n < 1 || n >= 1000 {
+			fmt.Print("Número inválido. Digite um número entre 1 e 1000: ")
+			fmt.Scan(&n)
+		 } else {
+			break
+		}
+	}
+
+	// Criando e preenchendo a lista numérica
+	v := make([]int, n)
+
+	fmt.Print("Digite os elementos da lista: \n")
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&v[i])
+	}
+
+	// Ordenando a lista:
+	sort.Ints(v)
+
+	// Imprimindo a lista inversa
+	fmt.Print(v)
+
+}
