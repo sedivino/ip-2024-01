13cbe9ddc46690baa19d85428fb6ce115704ffac
sedivino <165086372+sedivino@users.noreply.github.com>
2024-06-28 20:01:19 -0300

    Update q3.go
    up

--- teste03/q3.go
+++ teste03/q3.go
@@ -1,10 +1,44 @@
-func BubbleSort(array[] int)[]int {
-   for i:=0; i< len(array)-1; i++ {
-      for j:=0; j < len(array)-i-1; j++ {
-         if (array[j] > array[j+1]) {
-            array[j], array[j+1] = array[j+1], array[j]
-         }
-      }
-   }
-   return array
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		n int
+	)
+	//Criando e preenchendo um vetor com n números inteiros:
+
+	fmt.Print("Digite o tamanho do vetor: ")
+	fmt.Scan(&n)
+
+	v := make([]int, n)
+
+	fmt.Print("Digite os elementos do vetor: ")
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&v[i])
+	}
+
+	fmt.Println("Vetor original: ", v)
+
+   //Invertendo o vetor v de números inteiros:
+	bubbleSort(v)
+
+	fmt.Println("Vetor ordenado: ", v)
+
 }
+
+func bubbleSort(vetor []int) {
+	n := len(vetor)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if vetor[j] > vetor[j+1] {
+				// Troca os elementos se estiverem fora de ordem
+				vetor[j], vetor[j+1] = vetor[j+1], vetor[j]
+			}
+		}
+	}
+}
+
