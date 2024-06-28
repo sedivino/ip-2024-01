204000db48b553c4e172259f870c4fbafcb10ef7
sedivino <165086372+sedivino@users.noreply.github.com>
2024-06-28 19:40:03 -0300

    Create q3.go
    q3

---
+++ teste03/q3.go
@@ -0,0 +1,10 @@
+func BubbleSort(array[] int)[]int {
+   for i:=0; i< len(array)-1; i++ {
+      for j:=0; j < len(array)-i-1; j++ {
+         if (array[j] > array[j+1]) {
+            array[j], array[j+1] = array[j+1], array[j]
+         }
+      }
+   }
+   return array
+}
